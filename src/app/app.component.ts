bbb222f0b055bcc6ed26cebdb3215e367476930d
Aytugan Özgan <aytgnozgn@gmail.com>
2021-10-08 10:50:21 +0300

    video 68 sonu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   serverElements = [{ type: "server", name: "testServer", content: "Just a test" }]; // array of objects
 
+  onServerAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -6,32 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  @Output() blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   newServerName = '';
   newServerContent = '';
-
   constructor() { }
-
   ngOnInit(): void {
   }
-
   onAddServer() {
-    /*
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
     });
-     */
-  }
 
+  }
   onAddBlueprint() {
-    /**
-     this.serverElements.push({
-       type: 'blueprint',
-       name: this.newServerName,
-       content: this.newServerContent
-     }); s
-     */
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    })
   }
 
 }
